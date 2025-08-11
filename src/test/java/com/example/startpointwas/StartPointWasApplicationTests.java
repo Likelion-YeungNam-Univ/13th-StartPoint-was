48602313e6c06de9113c2ca1a81338a8ed97fee8
chamingyeong <mingyeong1104@naver.com>
2025-08-12 00:40:01 +0900

    chore setup

---
+++ src/test/java/com/example/startpointwas/StartPointWasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.startpointwas;
+
+import org.junit.jupiter.api.Test;
+//import org.springframework.boot.test.context.SpringBootTest;
+
+//@SpringBootTest
+class StartPointWasApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
