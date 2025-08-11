519224c1c79130dc3a32786befb0a2996c3db74b
chamingyeong <mingyeong1104@naver.com>
2025-08-12 00:57:55 +0900

    chore folder

--- src/main/java/startpointwas/StartPointWasApplication.java
+++ src/main/java/startpointwas/StartPointWasApplication.java
@@ -1,4 +1,4 @@
-package com.example.startpointwas;
+package startpointwas;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/startpointwas/StartPointWasApplicationTests.java
+++ src/test/java/startpointwas/StartPointWasApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.startpointwas;
+package startpointwas;
 
 import org.junit.jupiter.api.Test;
 //import org.springframework.boot.test.context.SpringBootTest;
