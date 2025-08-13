f2d9a3b7a10334a2377208a3cd8de4a45a011965
JHEEE116 <sjhyt0813@gmail.com>
2025-08-13 12:14:12 +0900

    feat/#18 사용자 엔티티 추가 (#40)
    Entity Role class add

---
+++ 13th-StartPoint-was-develop /src/main/java/startpointwas/domain/general/controller/SimpleAnlsController.java
@@ -0,0 +1,26 @@
+package startpointwas.domain.general.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import startpointwas.domain.general.dto.SimpleAnlsResponse;
+import startpointwas.domain.general.service.SimpleAnlsService;
+
+@RestController
+@RequestMapping("/api/general")
+public class SimpleAnlsController {
+
+    private final SimpleAnlsService service;
+
+    public SimpleAnlsController(SimpleAnlsService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<SimpleAnlsResponse> get(
+            @RequestParam String admiCd,
+            @RequestParam String upjongCd,
+            @RequestParam String simpleLoc
+    ) {
+        return ResponseEntity.ok(service.fetchAvgAmtInfo(admiCd, upjongCd, simpleLoc));
+    }
+}
