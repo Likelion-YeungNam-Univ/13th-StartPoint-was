b1c1c5311cbef2885ead5ab4f3f55b7bae249ec6
JHEEE116 <sjhyt0813@gmail.com>
2025-08-13 21:00:09 +0900

    [feat/#37]-사용자 컨트롤러

---
+++ src/main/java/startpointwas/domain/user/controller/UserController.java
@@ -0,0 +1,36 @@
+package startpointwas.domain.user.controller;
+
+=
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import startpointwas.domain.user.dto.LoginDto;
+import startpointwas.domain.user.dto.NameRoleRes;
+import startpointwas.domain.user.dto.SignUpReq;
+import startpointwas.domain.user.dto.UserInfoDto;
+import startpointwas.domain.user.entity.User;
+import startpointwas.domain.user.service.UserService;
+
+@RestController
+@RequestMapping("/api/users")
+@RequiredArgsConstructor
+public class UserController {
+
+    private static final String SESSION_KEY = "LOGIN_USER_ID";
+    private final UserService userService;
+
+
+    @PostMapping("/signup")
+    public ResponseEntity<NameRoleRes> signUp(@Valid @RequestBody SignUpReq req) {
+        User saved = userService.signUp(req);
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(NameRoleRes.from(saved));
+    }
+    
+}
