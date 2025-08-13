c02e32b1d58bcea5abf60fb0c60a0efc2a38f05b
JHEEE116 <sjhyt0813@gmail.com>
2025-08-13 14:31:35 +0900

    choreset

---
+++ 13th-StartPoint-was/src/main/java/startpointwas/domain/user/entity/Role.java
@@ -0,0 +1,12 @@
+package startpointwas.domain.user.entity;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public enum Role {
+    MENTOR("MENTOR"), MENTEE("MENTEE");
+
+    private final String value;
+}
