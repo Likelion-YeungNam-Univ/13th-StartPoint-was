d9df8180c70854e8df35ee643cd77628392d0547
JHEEE116 <sjhyt0813@gmail.com>
2025-08-13 20:22:20 +0900

    [feat/#32]-서비스-회원가입

---
+++ src/main/java/startpointwas/domain/user/service/UserService.java
@@ -0,0 +1,28 @@
+package startpointwas.domain.user.service;
+
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.ResponseCookie;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import startpointwas.domain.user.dto.SignUpReq;
+import startpointwas.domain.user.dto.UserInfoDto;
+import startpointwas.domain.user.entity.User;
+import startpointwas.domain.user.repository.UserRepository;
+
+@Service
+@RequiredArgsConstructor
+public class UserService {
+
+    private final UserRepository userRepository;
+
+    @Transactional
+    public User signUp(SignUpReq dto) {
+        User user = dto.toEntity(dto.getPassword());
+        return userRepository.save(user);
+    }
+
+
+}
