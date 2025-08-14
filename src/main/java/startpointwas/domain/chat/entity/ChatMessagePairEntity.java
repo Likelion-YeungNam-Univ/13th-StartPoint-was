c603ca6bb4eec1bcf7a5c4e9a6da3c2781e3bc32
JHEEE116 <sjhyt0813@gmail.com>
2025-08-14 21:17:31 +0900

    [feat/#14]-챗봇 리포지토리 엔티티 수정

--- src/main/java/startpointwas/domain/chat/entity/ChatMessagePairEntity.java
+++ src/main/java/startpointwas/domain/chat/entity/ChatMessagePairEntity.java
@@ -2,9 +2,13 @@ package startpointwas.domain.chat.entity;
 
 
 import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import java.time.Instant;
 
+@NoArgsConstructor
 @Entity
+@Getter
 @Table(
         name = "chat_message_pair",
         indexes = { @Index(name="idx_ctx_id_id", columnList = "contextId,id") }
@@ -13,6 +17,9 @@ public class ChatMessagePairEntity {
 
     @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(nullable = false)
+    private String userId;
 
     @Column(nullable = false)
     private String contextId;
@@ -25,14 +32,11 @@ public class ChatMessagePairEntity {
 
     private Instant createdAt = Instant.now();
 
-    protected ChatMessagePairEntity() {}
-    public ChatMessagePairEntity(String contextId, String q, String a) {
-        this.contextId = contextId; this.question = q; this.answer = a;
-    }
 
-    public Long getId() { return id; }
-    public String getContextId() { return contextId; }
-    public String getQuestion() { return question; }
-    public String getAnswer() { return answer; }
-    public Instant getCreatedAt() { return createdAt; }
+    public ChatMessagePairEntity(String userId, String contextId, String q, String a) {
+        this.userId = userId;
+        this.contextId = contextId;
+        this.question = q;
+        this.answer = a;
+    }
 }
