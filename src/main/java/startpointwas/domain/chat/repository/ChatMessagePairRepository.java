63b623115c618940117a817af4bf9e123810d0f5
JHEEE116 <sjhyt0813@gmail.com>
2025-08-14 23:12:15 +0900

    [feat/#14]-챗봇 엔티티 및 리포지토리 수정  (#65)
    * [feat/#14]-챗봇 엔티티 추가
    
    * [feat/39]-redis환경설정
    
    * [fix/#14]-yml주석삭제
    
    * [feat/#14]-챗봇 립포지토리 구성
    
    * [feat/#14]-챗봇 리포지토리 구성 및 불필요 테스트 코드 삭제
    
    * [feat/#14]-챗봇 리포지토리 엔티티 수정

--- src/main/java/startpointwas/domain/chat/repository/ChatMessagePairRepository.java
+++ src/main/java/startpointwas/domain/chat/repository/ChatMessagePairRepository.java
@@ -2,17 +2,10 @@ package startpointwas.domain.chat.repository;
 
 
 import startpointwas.domain.chat.entity.ChatMessagePairEntity;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import java.util.List;
 
 public interface ChatMessagePairRepository extends JpaRepository<ChatMessagePairEntity, Long> {
-
-    Page<ChatMessagePairEntity> findByContextIdOrderByIdDesc(String contextId, Pageable pageable);
-
-    List<ChatMessagePairEntity> findTop200ByContextIdOrderByIdDesc(String contextId);
-
+    List<ChatMessagePairEntity> findByUserIdOrderByIdAsc(String userId);
     long deleteByContextId(String contextId);
 }
