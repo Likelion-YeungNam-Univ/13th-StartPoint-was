4222ba3b3dc8b606888a86cf6546d8ca1d7f86d6
chamingyeong <mingyeong1104@naver.com>
2025-08-17 04:30:51 +0900

    [feat/10]-api-path-modified

--- src/main/java/startpointwas/domain/practical/controller/PracticalController.java
+++ src/main/java/startpointwas/domain/practical/controller/PracticalController.java
@@ -17,7 +17,7 @@ import java.util.List;
 import java.util.Map;
 
 @RestController
-@RequestMapping("/api/ai/practical")
+@RequestMapping("/api/practical")
 @RequiredArgsConstructor
 public class PracticalController {
 
