857a21bf9b850005a3733177d82a2861115ebe11
chamingyeong <mingyeong1104@naver.com>
2025-08-17 02:41:18 +0900

    [feat/10]-dto-slim

--- src/main/java/startpointwas/domain/practical/dto/PracticalDongAnlsSlim.java
+++ src/main/java/startpointwas/domain/practical/dto/PracticalDongAnlsSlim.java
@@ -6,34 +6,15 @@ import java.util.List;
 @Builder
 @Getter
 @Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class PracticalDongAnlsSlim {
     private String admiCd;
-    private String upjongCd;
     private String stdYm;
-
     private Integer dayAvg;
-    private Double firstHour;
-    private Double secondHour;
-    private Double thirdHour;
-    private Double fourthHour;
-    private Double fifthHour;
-    private Double sixthHour;
-    private Double tues;
-    private Double wed;
-    private Double thur;
-    private Double fri;
-    private Double sat;
-    private Double sun;
-    private Double weekend;
-    private Double day;
-
     private Integer saleAmt;
     private Integer saleCnt;
-
-    private Double prevMonRate;
     private Double prevYearRate;
-    private Double prevMonCntRate;
-    private Double prevYearCntRate;
     private Integer storeCntAdminNow;
 
 }
