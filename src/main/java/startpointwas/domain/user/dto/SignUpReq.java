c496b4b9a0efbaadcba2f8804030a99a12ca6a07
JHEEE116 <sjhyt0813@gmail.com>
2025-08-17 05:35:13 +0900

    Fix/35 schema delete (#83)
    * [fix/#34]-널 체크 제거
    
    * [fix/#35]-스키마 삭제

--- src/main/java/startpointwas/domain/user/dto/SignUpReq.java
+++ src/main/java/startpointwas/domain/user/dto/SignUpReq.java
@@ -11,25 +11,18 @@ import java.time.LocalDate;
 @Schema(description = "회원가입 요청 API")
 public class SignUpReq {
 
-    @Schema(description = "사용자 이름", example = "홍길동")
     private String name;
 
-    @Schema(description = "로그인ID/유저ID", example = "lion13")
     private String userId;
 
-    @Schema(description = "비밀번호", example = "abcd1234")
     private String password;
 
-    @Schema(description = "생년월일", example = "2000-01-13")
     private LocalDate birth;
 
-    @Schema(description = "이메일", example = "likelion13@gmail.com")
     private String email;
 
-    @Schema(description = "역할", example = "MENTOR/MENTEE")
     private String role;
 
-    @Schema(description = "전화번호", example = "010-1234-5678")
     private String phone;
 
     public User toEntity(String encodedPassword) {
