4572fcd2498c76c10f7854fdf850a7a45be98386
chamingyeong <mingyeong1104@naver.com>
2025-08-17 11:06:33 +0900

    feat/90-http-setting

---
+++ src/main/java/startpointwas/config/WebConfig.java
@@ -0,0 +1,17 @@
+package startpointwas.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOriginPatterns("*")
+                .allowedMethods("GET", "POST", "FETCH")
+                .allowCredentials(true)
+                .maxAge(3600);
+    }
+}
